81166e66a1945934de6d3d624d890f03c5221b69
Robert Franke <franke.rob@gmail.com>
2019-12-11 21:01:31 +0100

    Day 10, part 2

--- 10/run.py
+++ 10/run.py
@@ -1,17 +1,23 @@
 import sys
 import math
+from collections import defaultdict
+from operator import itemgetter
 
 def count_visible(space, pos):
     posx, posy = pos
-    angles = set()
+    angles = defaultdict(list)
     for y in range(len(space)):
         for x in range(len(space[0])):
             if space[y][x] == ".":
                 continue
             if posx == x and posy == y:
                 continue
-            angles.add(math.atan2(posy - y, posx - x))
-    return len(angles)
+            # Angle to neg y-axis in [0, 2 * pi]
+            angles[math.fmod(math.atan2(posx - x, posy - y) + math.tau, math.tau)].append((x,y))
+    return len(angles), angles
+
+def distsq(pos1, pos2):
+    return (pos1[0] - pos2[0])**2 + (pos1[1] - pos2[1])**2
 
 def main():
     infn = "input.txt"
@@ -26,11 +32,24 @@ def main():
         for x in range(len(space[0])):
             if space[y][x] == ".":
                 continue
-            count = count_visible(space, (x, y))
+            count, _ = count_visible(space, (x, y))
             if count > max_asteroids:
                 max_asteroids = count
                 bestpos = (x,y)
     print(max_asteroids, bestpos)
+
+    _, angles = count_visible(space, bestpos)
+    angles = list(sorted(angles.items(), key=itemgetter(0)))
+    for _, asteroids in angles:
+        asteroids.sort(key= lambda a: distsq(bestpos, a))
+    vaporized = 0
+    i = 0
+    while vaporized < 200:
+        if angles[i]:
+            vaporized += 1
+            print(vaporized, angles[i][1][0])
+            angles[i][1].pop(0)
+        i = (i - 1) % len(angles)
 
 if __name__ == "__main__":
     main()
---
+++ 11/run.py
@@ -0,0 +1,137 @@
+import itertools
+import sys
+
+def debug(*args):
+    if DEBUG:
+        print(*args)
+
+DEBUG = False
+
+class Program:
+    def __init__(self, opcodes, input, output):
+        self.opcodes = opcodes[:] + [0] * 1000000
+        self.input = input
+        self.output = output
+        self.ip = 0
+        self.relative_base = 0
+        self.stopped = False
+
+    def get_value(self, ip, mode):
+        if mode == 0:
+            return self.opcodes[self.opcodes[ip]]
+        elif mode == 1:
+            return self.opcodes[ip]
+        elif mode == 2:
+            return self.opcodes[self.relative_base + self.opcodes[ip]]
+        else:
+            raise ValueError("Unknown mode {}".format(mode))
+
+    def set_value(self, ip, mode, value):
+        assert mode != 1
+        if mode == 0:
+            self.opcodes[self.opcodes[ip]] = value
+        elif mode == 2:
+            self.opcodes[self.relative_base + self.opcodes[ip]] = value
+        else:
+            raise ValueError("Unknown mode {}".format(mode))
+
+    def step(self):
+        if self.stopped:
+            return False
+        opcodes = self.opcodes
+        op = opcodes[self.ip]
+        opcode = op % 100
+        debug("Op: ", op)
+        if opcode == 1:
+            # Add
+            mode1 = (op//100) % 10
+            mode2 = (op//1000) % 10
+            mode3 = (op//10000) % 10
+            op1 = self.get_value(self.ip + 1, mode1)
+            op2 = self.get_value(self.ip + 2, mode2)
+            self.set_value(self.ip + 3, mode3, op1 + op2)
+            self.ip += 4
+        elif opcode == 2:
+            # Mul
+            mode1 = (op//100) % 10
+            mode2 = (op//1000) % 10
+            mode3 = (op//10000) % 10
+            op1 = self.get_value(self.ip + 1, mode1)
+            op2 = self.get_value(self.ip + 2, mode2)
+            self.set_value(self.ip + 3, mode3, op1 * op2)
+            self.ip += 4
+        elif opcode == 3:
+            # Input
+            mode1 = (op//100) % 10
+            self.set_value(self.ip + 1, mode1, self.input.pop(0))
+            self.ip += 2
+        elif opcode == 4:
+            # Output
+            mode = (op//100) % 10
+            val = self.get_value(self.ip + 1, mode)
+            self.output.append(val)
+            self.ip += 2
+        elif opcode == 5:
+            # Not equal 0
+            mode1 = (op//100) % 10
+            mode2 = (op//1000) % 10
+            op1 = self.get_value(self.ip + 1, mode1)
+            op2 = self.get_value(self.ip + 2, mode2)
+            if op1 != 0:
+                self.ip = op2
+            else:
+                self.ip += 3
+        elif opcode == 6:
+            # Equal 0
+            mode1 = (op//100) % 10
+            mode2 = (op//1000) % 10
+            op1 = self.get_value(self.ip + 1, mode1)
+            op2 = self.get_value(self.ip + 2, mode2)
+            if op1 == 0:
+                self.ip = op2
+            else:
+                self.ip += 3
+        elif opcode == 7:
+            # Less than
+            mode1 = (op//100) % 10
+            mode2 = (op//1000) % 10
+            mode3 = (op//10000) % 10
+            op1 = self.get_value(self.ip + 1, mode1)
+            op2 = self.get_value(self.ip + 2, mode2)
+            self.set_value(self.ip + 3, mode3, int(op1 < op2))
+            self.ip += 4
+        elif opcode == 8:
+            # Equal
+            mode1 = (op//100) % 10
+            mode2 = (op//1000) % 10
+            mode3 = (op//10000) % 10
+            op1 = self.get_value(self.ip + 1, mode1)
+            op2 = self.get_value(self.ip + 2, mode2)
+            self.set_value(self.ip + 3, mode3, int(op1 == op2))
+            self.ip += 4
+        elif opcode == 9:
+            mode1 = (op//100) % 10
+            op1 = self.get_value(self.ip + 1, mode1)
+            self.relative_base += op1
+            self.ip += 2
+        elif opcode == 99:
+            self.stopped = True
+            return False
+        else:
+            raise RuntimeError("Unknown opcode %d at %d" % (opcode, self.ip))
+        return True
+
+def main():
+    infile = "input.txt"
+    if len(sys.argv) > 1:
+        infile = sys.argv[1]
+    program = [int(op) for op in open(infile).read().replace('\n', '').split(",")]
+
+    output = []
+    p = Program(program, input=[1], output=output)
+    while p.step():
+        pass
+    print(output)
+
+if __name__ == "__main__":
+    main()
