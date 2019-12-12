dd5659fa1ae201005e79156ac305a9992f7f36c1
Robert Franke <franke.rob@gmail.com>
2019-12-12 21:29:53 +0100

    Day 11

--- 11/run.py
+++ 11/run.py
@@ -1,5 +1,7 @@
 import itertools
 import sys
+from collections import defaultdict
+from operator import itemgetter
 
 def debug(*args):
     if DEBUG:
@@ -63,13 +65,13 @@ class Program:
         elif opcode == 3:
             # Input
             mode1 = (op//100) % 10
-            self.set_value(self.ip + 1, mode1, self.input.pop(0))
+            self.set_value(self.ip + 1, mode1, self.input.read())
             self.ip += 2
         elif opcode == 4:
             # Output
             mode = (op//100) % 10
             val = self.get_value(self.ip + 1, mode)
-            self.output.append(val)
+            self.output.write(val)
             self.ip += 2
         elif opcode == 5:
             # Not equal 0
@@ -121,17 +123,68 @@ class Program:
             raise RuntimeError("Unknown opcode %d at %d" % (opcode, self.ip))
         return True
 
+BLACK = 0
+WHITE = 1
+
+LEFT = 0
+RIGHT = 1
+
+PAINT = 0
+TURN = 1
+# current_dir -> (dir after left turn, dir after right turn)
+DIR_TRANS = {
+    (0,1): ((-1,0), (1,0)),
+    (1,0): ((0,1), (0,-1)),
+    (0,-1): ((1,0), (-1,0)),
+    (-1,0): ((0,-1), (0,1))
+}
+class Hull:
+    def __init__(self):
+        self.hull = defaultdict(lambda: BLACK)
+        self.robot_pos = (0, 0)
+        self.robot_dir = (0, 1) # Facing up 
+        self.mode = PAINT
+
+    def read(self):
+        return self.hull[self.robot_pos]
+
+    def write(self, val):
+        if self.mode == PAINT:
+            self.hull[self.robot_pos] = val
+            self.mode = TURN
+        elif self.mode == TURN:
+            self.robot_dir = DIR_TRANS[self.robot_dir][val]
+            self.robot_pos = (self.robot_pos[0] + self.robot_dir[0], self.robot_pos[1] + self.robot_dir[1])
+            self.mode = PAINT
+        else:
+            raise ValueError("Unknown val {:d}".format(val))
+
+    def print(self):
+        x_min = min(self.hull.keys(), key=itemgetter(0))[0]
+        x_max = max(self.hull.keys(), key=itemgetter(0))[0]
+        y_min = min(self.hull.keys(), key=itemgetter(1))[1]
+        y_max = max(self.hull.keys(), key=itemgetter(1))[1]
+        for y in range(y_max + 1, y_min - 2, -1):
+            print(''.join("#" if self.hull[(x, y)]==BLACK else " " for x in range(x_min, x_max + 1)))
+    
+
 def main():
     infile = "input.txt"
     if len(sys.argv) > 1:
         infile = sys.argv[1]
     program = [int(op) for op in open(infile).read().replace('\n', '').split(",")]
 
-    output = []
-    p = Program(program, input=[1], output=output)
+    hull = Hull()
+    p = Program(program, input=hull, output=hull)
     while p.step():
         pass
-    print(output)
+    print(len(hull.hull))
+    hull = Hull()
+    hull.hull[(0,0)] = WHITE
+    p = Program(program, input=hull, output=hull)
+    while p.step():
+        pass
+    hull.print()
 
 if __name__ == "__main__":
     main()
